26fe87101125beda238267d12fdaf7c63887d36e
Mae Pena <93175349+maepena91@users.noreply.github.com>
2022-05-23 20:29:09 -0700

    Update bugs_controller.js

--- backend/controllers/bugs_controller.js
+++ backend/controllers/bugs_controller.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const router = require("express").Router();
 const Bug = require("../models/bug.js");
-const User = require("../models/User.js");
+// const User = require("../models/User.js");
 
 // Create bug route
 router.post("/", async (req, res) => {
-    try {
-        const Bugs = await Bug.create(req.body);
-        // res.redirect("/bugs");
-        console.log(Bugs);
-        res.send(Bugs);
-    } catch (e) {
-        console.log(e.message);
-        res.send(e)
-    }
+  try {
+    const Bugs = await Bug.create(req.body);
+    // res.redirect("/bugs");
+    console.log(Bugs);
+    res.send(Bugs);
+  } catch (e) {
+    console.log(e.message);
+    res.send(e)
+  }
 });
-
+// Update specific bug
 router.put("/:id", async (req, res) => {
   try {
-    const Bugs = await Bug.findByIdAndUpdate(req.params.id,req.body);
+    const Bugs = await Bug.findByIdAndUpdate(req.params.id, req.body);
     // res.redirect("/bugs");
     console.log(Bugs);
     res.send(Bugs);
@@ -27,8 +27,8 @@ router.put("/:id", async (req, res) => {
     res.send(e);
   }
 });
-
-router.get("/", async (req, res) => {
+// get all bugs
+router.get("/getbugs", async (req, res) => {
   try {
     const Bugs = await Bug.find();
     console.log(Bugs);
@@ -39,6 +39,48 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/low", async (req, res) => {
+  try {
+    const Bugs = await Bug.find({ priority: "low" });
+    console.log(Bugs);
+    res.send(Bugs);
+  } catch (e) {
+    console.log(e.message);
+    res.send(e);
+  }
+});
+
+router.get("/medium", async (req, res) => {
+  try {
+    const Bugs = await Bug.find({ priority: "medium" });
+    console.log(Bugs);
+    res.send(Bugs);
+  } catch (e) {
+    console.log(e.message);
+    res.send(e);
+  }
+});
+// Get all HIGH PRIORITY bugs
+router.get("/high", async (req, res) => {
+  try {
+    const Bugs = await Bug.find({ priority: "high" });
+    console.log(Bugs);
+    res.send(Bugs);
+  } catch (e) {
+    console.log(e.message);
+    res.send(e);
+  }
+});
+
+// Delete specific bug
+router.delete("/delete/:bugId", async (req, res) => {
+  try {
+    const foundBug = await Bug.findByIdAndDelete(req.params.bugId);
+    console.log(foundBug);
+  } catch (e) { console.log(e.message); res.send(e); }
+})
+
+// get specific bug
 router.get("/:bugId", async (req, res) => {
   try {
     const foundBug = await Bug.findById(req.params.bugId);
@@ -50,12 +92,7 @@ router.get("/:bugId", async (req, res) => {
   }
 });
 
-router.delete("/delete/:bugId", async (req,res)=>{
-    try {
-        const foundBug  = await Bug.findByIdAndDelete(req.params.bugId);
-        console.log(foundBug);  
-    }catch (e){console.log(e.message); res.send(e);}
-})
+
 
 
 module.exports = router
